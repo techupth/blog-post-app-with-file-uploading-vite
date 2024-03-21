cdf1e8b4e400f57d57f84a72f9f229d44887f916
Park <tanagrit.sornchaiyagorn@gmail.com>
2024-03-21 15:51:44 +0700

    modify: change cra to vite

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { useAuth } from "./contexts/authentication.jsx";
+import AuthenticatedApp from "./pages/AuthenticatedApp.jsx";
+import UnauthenticatedApp from "./pages/UnauthenticatedApp.jsx";
+
+function App() {
+  const auth = useAuth();
+  return auth.isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+}
+
+export default App;
